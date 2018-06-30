6a1d9c0be1c2f4542e8dc158592727d27516823e
social-anthrax <social-anthrax@users.noreply.github.com>
2018-06-30 15:14:43 +0100

    Now with comments!

--- emoji.py
+++ emoji.py
@@ -1,23 +1,25 @@
-import sys
+import sys #neccessary for sys.argv
 
 def proccess (text):
 
-    newWords = []
+    newWords = [] #initialises aray that will hold the modified letters
 
-    for words in list(list(text[1:])):
-        words = words + ' '
-        charofword = list(words)
+    for words in list(list(text[1:])): #ARRAYS START AT 1 REEEEE. Jokes. This is used as the name of the program is normaly also taken in as an argument. This is to stop that being sent as input.
 
-        for letter in charofword:
-            if letter != ' ':
-                newWords.append(':') #adds : to the end of the array
+        words = words + ' ' #Adds space at the end of the words so that there is a space when you copy over
+
+        charofword = list(words) #Creates a character array
+
+        for letter in charofword: #Itterates for every character in the charofword array (including space.)
+            if letter != ' ': #This is to stop spaces being modified.
+                newWords.append(':') #Adds : to the end of the array
                 newWords.append('regional_indicator_' + letter.lower())
-                newWords.append(':'+' ')
+                newWords.append(':'+' ') #Neccesary as discord has a spaz if u dont add a space.
             else:
-                newWords.append(' '*3)
+                newWords.append(' '*3) #Makes spaces larger to make them actually visible
 
 
-    sentence = "".join(newWords)
-    return sentence
+    sentence = "".join(newWords) #Makes the newWords array a string.
+    return sentence #returns the value
 
-print (proccess(sys.argv))
+print (proccess(sys.argv)) #"sys.argv" takes arguments from the command line.
